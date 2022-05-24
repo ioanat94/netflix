40981e282df7a613636aeb9f9d2070d82981b349
ioanat94 <ioanatiplea94@gmail.com>
2022-05-24 14:35:11 +0300

    Finish sliders

--- src/components/List.jsx
+++ src/components/List.jsx
@@ -2,16 +2,36 @@ import {
   ArrowBackIosOutlined,
   ArrowForwardIosOutlined,
 } from '@mui/icons-material';
-import React from 'react';
+import React, { useRef, useState } from 'react';
 import ListItem from './ListItem';
 
 function List() {
+  const [isMoved, setIsMoved] = useState(false);
+
+  const listRef = useRef();
+
+  const handleClick = (direction) => {
+    setIsMoved(true);
+    let distance = listRef.current.getBoundingClientRect().x - 65;
+    if (direction === 'left') {
+      listRef.current.style.transform = `translateX(${248 + distance}px)`;
+    } else if (direction === 'right') {
+      listRef.current.style.transform = `translateX(${-248 + distance}px)`;
+    }
+  };
+
   return (
     <div className='list'>
       <span className='listTitle'>Continue Watching</span>
       <div className='wrapper'>
-        <ArrowBackIosOutlined className='sliderArrow left' />
-        <div className='container'>
+        <div className='arrowWrapper left'>
+          <ArrowBackIosOutlined
+            className='sliderArrow left'
+            onClick={() => handleClick('left')}
+            style={{ display: !isMoved && 'none' }}
+          />
+        </div>
+        <div className='container' ref={listRef}>
           <ListItem />
           <ListItem />
           <ListItem />
@@ -23,7 +43,12 @@ function List() {
           <ListItem />
           <ListItem />
         </div>
-        <ArrowForwardIosOutlined className='sliderArrow right' />
+        <div className='arrowWrapper right'>
+          <ArrowForwardIosOutlined
+            className='sliderArrow right'
+            onClick={() => handleClick('right')}
+          />
+        </div>
       </div>
     </div>
   );
--- src/components/ListItem.jsx
+++ src/components/ListItem.jsx
@@ -1,7 +1,19 @@
+import { Add, PlayArrow, ThumbUpOffAltOutlined } from '@mui/icons-material';
 import React from 'react';
 
 function ListItem() {
-  return <div className='listItem'></div>;
+  return (
+    <div className='listItem'>
+      <img src={require('../images/chefs_table.jfif')} alt='Show Preview' />
+      <div className='itemInfo'>
+        <div className='icons'>
+          <PlayArrow />
+          <Add />
+          <ThumbUpOffAltOutlined />
+        </div>
+      </div>
+    </div>
+  );
 }
 
 export default ListItem;
