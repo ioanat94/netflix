4a23573e8608ed684bb612a68417e1fb4745f231
ioanat94 <ioanatiplea94@gmail.com>
2022-05-25 11:36:33 +0300

    Created login page

--- src/App.js
+++ src/App.js
@@ -1,11 +1,12 @@
 import Home from './pages/Home';
 import Register from './pages/Register';
 import Watch from './pages/Watch';
+import Login from './pages/Login';
 
 function App() {
   return (
     <div className='App'>
-      <Register />
+      <Login />
     </div>
   );
 }
