94fdcd3762bd293c13a1597bf82c5f3a17ca0ad2
ioanat94 <ioanatiplea94@gmail.com>
2022-05-30 13:12:20 +0300

    Created auth routes

--- api/index.js
+++ api/index.js
@@ -2,6 +2,8 @@ const express = require('express');
 const app = express();
 const mongoose = require('mongoose');
 const dotenv = require('dotenv');
+const authRoute = require('./routes/auth');
+
 dotenv.config();
 
 mongoose
@@ -11,6 +13,10 @@ mongoose
   })
   .then(() => console.log('DB connection successful...'))
   .catch((err) => console.log(err));
+
+app.use(express.json());
+
+app.use('/api/auth', authRoute);
 
 app.listen(8800, () => {
   console.log('Backend server is running...');
---
+++ api/routes/auth.js
@@ -0,0 +1,52 @@
+const router = require('express').Router();
+const User = require('../models/User');
+const CryptoJS = require('crypto-js');
+const jwt = require('jsonwebtoken');
+
+// Register
+router.post('/register', async (req, res) => {
+  const newUser = new User({
+    username: req.body.username,
+    email: req.body.email,
+    password: CryptoJS.AES.encrypt(
+      req.body.password,
+      process.env.SECRET_KEY
+    ).toString(),
+  });
+
+  try {
+    const user = await newUser.save();
+    res.status(201).json(user);
+  } catch (err) {
+    res.status(500).json(err);
+  }
+});
+
+// Login
+router.post('/login', async (req, res) => {
+  try {
+    const user = await User.findOne({ email: req.body.email });
+    if (!user) {
+      !user && res.status(401).json('Wrong email or password');
+    } else {
+      const bytes = CryptoJS.AES.decrypt(user.password, process.env.SECRET_KEY);
+      const decrypted = bytes.toString(CryptoJS.enc.Utf8);
+
+      if (decrypted !== req.body.password) {
+        res.status(401).json('Wrong email or password');
+      } else {
+        const accessToken = jwt.sign(
+          { id: user._id, isAdmin: user.isAdmin },
+          process.env.SECRET_KEY,
+          { expiresIn: '5d' }
+        );
+        const { password, ...info } = user._doc;
+        res.status(200).json({ ...info, accessToken });
+      }
+    }
+  } catch (err) {
+    res.status(500).json(err);
+  }
+});
+
+module.exports = router;
