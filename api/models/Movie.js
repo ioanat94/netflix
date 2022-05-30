5d65591f0ad54fb6248071167334d2f64137cedf
ioanat94 <ioanatiplea94@gmail.com>
2022-05-30 10:14:16 +0300

    Created user, movie and list models

---
+++ api/models/Movie.js
@@ -0,0 +1,20 @@
+const mongoose = require('mongoose');
+
+const MovieSchema = new mongoose.Schema(
+  {
+    title: { type: String, required: true, unique: true },
+    description: { type: String },
+    image: { type: String },
+    imageTitle: { type: String },
+    imageSmall: { type: String },
+    trailer: { type: String },
+    video: { type: String },
+    year: { type: String },
+    limit: { type: Number },
+    genre: { type: String },
+    isSeries: { type: Boolean, default: false },
+  },
+  { timestamps: true }
+);
+
+module.export = mongoose.model('Movie', MovieSchema);
