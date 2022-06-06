1ca06feb24cd21fd3cc5d531d09d850582cfc572
ioanat94 <ioanatiplea94@gmail.com>
2022-06-06 16:38:07 +0300

    Fix inability to login

--- client/src/authContext/apiCalls.js
+++ client/src/authContext/apiCalls.js
@@ -4,7 +4,7 @@ import { loginFailure, loginStart, loginSuccess, logout } from './AuthActions';
 export const loginCall = async (user, dispatch) => {
   dispatch(loginStart());
   try {
-    const res = await axios.post('/login', user);
+    const res = await axios.post('/auth/login', user);
     dispatch(loginSuccess(res.data));
   } catch (err) {
     dispatch(loginFailure());
